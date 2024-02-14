252e401bd95d0eab57dc846ec06c5442a2992bd7
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-02-14 12:48:16 +0300

    Added comments for POINT2

--- include/POINT2.hpp
+++ include/POINT2.hpp
@@ -1,13 +1,16 @@
 #include <Windows.h>
 
+/// @brief Extended version of Windows.h POINT structure
 struct POINT2 {
 public:
     LONG x, y;
 
+    /// @brief Create a POINT2 object, specifying the coordinates
     explicit POINT2(LONG x_ = 0, LONG y_ = 0) {
         x = x_;
         y = y_;
     }
+    /// @brief Create a POINT2 object using POINT object
     explicit POINT2(POINT point) {
         x = point.x;
         y = point.y;
